d1e0660d118603c8c248747417c530b100c13229
rushit5 <77121716+rushit5@users.noreply.github.com>
2021-01-11 14:45:27 -0500

    Update column_chart.js

--- column_chart.js
+++ column_chart.js
@@ -78,7 +78,7 @@ google.charts.setOnLoadCallback(drawChart);
         var options = {
 	  legend: {position: 'bottom'},
 	  vAxis: {gridlines:{count:0}},
-          chartArea:{left:60,top:0,width:'100%',height:'65%'},
+          chartArea:{left:60,top:10,width:'100%',height:'65%'},
           bar:{groupWidth:'85%'},
           chart: {
             title: '',
