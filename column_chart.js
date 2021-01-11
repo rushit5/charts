3f3eebcbe617e200034549212880649a4ebaf3ed
rushit5 <77121716+rushit5@users.noreply.github.com>
2021-01-11 12:20:43 -0500

    Update column_chart.js

--- column_chart.js
+++ column_chart.js
@@ -55,19 +55,21 @@ for(var row of data) {
 	]);
 }
 
-var plot_data = [['Q','Total Count','Goal']];
+var plot_data = [['Q','Total Count',{ role: "style" },'Goal',{ role: "style" }]];
 for(var row of data) {
 	var cell = row[queryResponse.fields.dimensions[0].name]
 	plot_data.push([ 
            row[x].value,
-	   row[y].value,'color :'.concat(row[a].value),
-	   row[z].value,'color : #aba9ad'
+	   row[y].value,
+	   'color :'.concat(row[a].value),
+	   row[z].value,
+	   'color : #aba9ad'
 	]);
 }
 
 console.log('Color', plot_data)	
 
-google.charts.load('current', {'packages':['bar']});
+google.charts.load('current', {'packages':['corechart', 'bar']});
 google.charts.setOnLoadCallback(drawChart);
 
       function drawChart() {
@@ -80,9 +82,9 @@ google.charts.setOnLoadCallback(drawChart);
           }
         };
 
-        var chart = new google.charts.Bar(document.getElementById('container'));
+        var chart = new google.charts.ColumnChart(document.getElementById('container'));
 
-        chart.draw(data, google.charts.Bar.convertOptions(options));
+        chart.draw(data, google.charts.ColumnChart.convertOptions(options));
       }
 
 doneRendering();
