fdd26c57205b1192fae18ceadc06e3f66cd0e66d
rushit5 <77121716+rushit5@users.noreply.github.com>
2021-01-11 11:34:10 -0500

    Update column_chart.js

--- column_chart.js
+++ column_chart.js
@@ -60,8 +60,8 @@ for(var row of data) {
 	var cell = row[queryResponse.fields.dimensions[0].name]
 	plot_data.push([ 
            row[x].value,
-	   {row[y].value,'color :'.concat(row[a].value)},
-	   {row[z].value,'color : #aba9ad'}
+	   row[y].value,'color :'.concat(row[a].value),
+	   row[z].value,'color : #aba9ad'
 	]);
 }
 
