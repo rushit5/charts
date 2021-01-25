71342ab622cae4b9d0bcceff8a0b3e7f28a58e3c
rushit5 <77121716+rushit5@users.noreply.github.com>
2021-01-25 10:34:36 -0500

    Create overlap_bar_pipeline_group.js

---
+++ overlap_bar_pipeline_group.js
@@ -0,0 +1,115 @@
+looker.plugins.visualizations.add({
+create: function(element, config) {
+  element.innerHTML = `
+	<style>
+	.sannith {
+	min-width: 200px;
+	height: 240px
+	}
+	</style>
+	`;
+	
+    var container = element.appendChild(document.createElement("div"));
+	container.className = "sannith";
+    container.id = 'container';
+		
+  },
+
+updateAsync: function(data, element, config, queryResponse, details, doneRendering) {
+// Clear any errors from previous updates:
+this.clearErrors();
+
+// Dump data and metadata to console:
+console.log('updateAsync() data', data)
+console.log('updateAsync() config', config)
+console.log('updateAsync() queryResponse', queryResponse)
+	
+// get the names of the first dimension and measure available in data
+
+x  = config.query_fields.dimensions[0].name;     // quater
+y  = config.query_fields.dimensions[1].name;     // total count
+z  = config.query_fields.dimensions[2].name;     // BM total count
+a  = config.query_fields.dimensions[3].name;     // color codes
+
+ 
+var quarter = [];
+for(var row of data) {
+	var cell = row[queryResponse.fields.dimensions[0].name]
+	quarter.push([
+		row[x].value 
+	]);
+}
+
+var tot_cnt = [];
+for(var row of data) {
+	var cell = row[queryResponse.fields.dimensions[1].name]
+	tot_cnt.push([
+		row[y].value 
+	]);
+}
+var bm_data = [];
+for(var row of data) {
+	var cell = row[queryResponse.fields.dimensions[2].name]
+	bm_data.push([
+		row[z].value 
+	]);
+}
+
+var plot_data = [];
+for(var row of data) {
+	var cell = row[queryResponse.fields.dimensions[0].name]
+	plot_data.push({
+	       y     : row[y].value,
+	       color : row[a].value  		      
+	});
+}
+
+console.log('Color', plot_data)	
+ 	 
+Highcharts.chart('container', {
+    chart: {
+        type: 'column'
+    },
+    title: {
+        text: ''
+    },
+    xAxis: {
+        categories: quarter
+    },
+    legend: {
+        shadow: false
+    },
+    tooltip: {
+        shared: true
+    },
+    plotOptions: {
+        column: {
+	    dataLabels: {
+                enabled: true,
+                allowOverlap:true,
+                y:10
+            },
+            grouping: false,
+            shadow: false,
+            borderWidth: 0
+        }
+    },
+    series: [{
+        name: 'Goal',
+        color: "#aba9ad",
+        data: bm_data,
+        dataLabels:false,
+        pointPadding: 0.2,
+        pointPlacement: 0
+    }, {
+        name: 'Total Count',
+	      color: "#44cf3a",
+        data: plot_data,
+        pointPadding: 0.3,
+        pointPlacement: 0
+    }]
+})
+ 
+doneRendering();
+}
+})
